ee1428d6d0724e8d9db2fd5f84c3424ccde16c25
Anton <XouST@Yandex.ru>
2024-01-28 13:48:49 +0300

    Initial commit

---
+++ Задание 6/Задание 6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите сумму вклада: ");
+        decimal sum = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Введите срок вклада в месяцах: ");
+        int period = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= period; i++)
+        {
+            sum += sum * 0.07;
+        }
+        Console.WriteLine($"После {period} месяцев сумма вклада составит {sum}");
+        Console.ReadKey();
+    }
+}
